7e090d42f4780909f1b17da0960f2c826071f35d
Wei Jiang <epichello12456@gmail.com>
2026-06-18 17:51:24 +1000

    Finished - Testing Required

--- ObstacleBot.ino
+++ ObstacleBot.ino
@@ -3,10 +3,8 @@
 
 #define IR_RECIEVE_PIN 9  //macro, defines and replaces anything with name ir_receive_pin to 9 if any
 
-// AF_DCMotor front_left(4); //Numbers corresponds to M1, M2 etc. on the shield
-// AF_DCMotor front_right(3);  //AF_DCMotor type for variables
-// AF_DCMotor back_left(1);  
-// AF_DCMotor back_right(2);
+AF_DCMotor left_side(4); //Numbers corresponds to M1, M2 etc. on the shield
+AF_DCMotor right_side(3);  //AF_DCMotor type for variables
 
 enum directions : unsigned long
 {
@@ -34,25 +32,9 @@ directions ir_sensor_data(directions &data)
 void setup() {
   Serial.begin(9600); //Opens communication to computer's serial monitor
   IrReceiver.begin(IR_RECIEVE_PIN, ENABLE_LED_FEEDBACK); //starts the receiver on pin 9, LED feedback makes uno blink everytime a signal is receive
-  // front_left.setSpeed(200); //max 255
-  // front_right.setSpeed(200);
-  // back_left.setSpeed(200);
-  // back_right.setSpeed(200);
-
-  // front_left.run(RELEASE);  //Sets motors to a stationary state
-  // front_right.run(RELEASE);
-  // back_left.run(RELEASE);
-  // back_right.run(RELEASE);
-
-  // front_right.run(FORWARD);
-  // front_left.run(FORWARD);
-  // back_left.run(FORWARD);
-  // back_right.run(FORWARD);
-  // delay(15000);
-  // front_right.run(RELEASE);
-  // front_left.run(RELEASE);
-  // back_left.run(RELEASE);
-  // back_right.run(RELEASE);
+  
+  left_side.setSpeed(255); //max 255
+  right_side.setSpeed(255);
 }
 
 void loop() {
@@ -63,33 +45,47 @@ void loop() {
     {
       case IR_RIGHT:
       {
-        Serial.println("Right");
+        left_side.run(FORWARD);
+        right_side.run(BACKWARD);
+        delay(500);
         ir_data = IR_NONE; 
+        left_side.run(RELEASE);
+        right_side.run(RELEASE);
         break;
       }
       case IR_LEFT:
       {
-        Serial.println("Left");
+        left_side.run(BACKWARD);
+        right_side.run(FORWARD);
+        delay(500);
+        ir_data = IR_NONE; 
+        left_side.run(RELEASE);
+        right_side.run(RELEASE);
         ir_data = IR_NONE;
         break;
       }
       case IR_FORWARD:
       {
-        Serial.println("aF");
+        left_side.run(FORWARD);
+        right_side.run(FORWARD);
+        delay(500);
+        ir_data = IR_NONE; 
+        left_side.run(RELEASE);
+        right_side.run(RELEASE);
         ir_data = IR_NONE;
         break;
       }
       case IR_BACKWARD:
       {
-        Serial.println("V");
-        ir_data = IR_NONE;
+        left_side.run(BACKWARD);
+        right_side.run(BACKWARD);
+        delay(500);
+        ir_data = IR_NONE; 
+        left_side.run(RELEASE);
+        right_side.run(RELEASE);
         break;
       }
     }
   }while(ir_data != IR_OFF);
   Serial.println("Control disconnected");
-//variable.run(FORWARD); 
-//variable.run(RELEASE);
-//variable.run(BACKWARD);
-//delay(1000); / 1000 ms = 1s
 }
